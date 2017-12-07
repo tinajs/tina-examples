20bdec961134214ab7e436fbfffd985e0746d84d
yelo <zhihuzeye@gmail.com>
2017-12-07 21:27:26 +0800

    feat: add sayhi project

---
+++ packages/sayhi/hi.page.js
@@ -0,0 +1,27 @@
+const { Page } = require('./tina.min.js')
+const { fetchUser } = require('./api')
+
+Page.define({
+  data: {
+    name: 'Tina',
+    surname: 'S',
+  },
+  compute ({ name, surname }) {
+    return {
+      fullname: `${name} ${surname}`
+    }
+  },
+  onLoad () {
+    fetchUser()
+      .then(({ name, surname }) => this.setData({ name, surname }))
+  },
+  methods: {
+    sayHi () {
+      wx.showModal({
+        title: 'Hi!',
+        content: this.data.fullname,
+        showCancel: false,
+      })
+    },
+  }
+})
