e1e6e3fecf3921944b32bcea8f6cf36063deed8a
yelo <zhihuzeye@gmail.com>
2017-12-17 04:21:35 +0800

    feat: import wxio

--- packages/sayhi-mina/src/api.js
+++ packages/sayhi-mina/src/api.js
@@ -1,9 +1,7 @@
+const wxio = require('wxio')
+
 exports.fetchUser = function () {
-  return new Promise((resolve, reject) => {
-    wx.request({
-      url: 'https://uinames.com/api/',
-      success: ({ data }) => resolve(data),
-      fail: reject,
-    })
-  })
+  return wxio.request({
+    url: 'https://uinames.com/api/',
+  }).then(({ data }) => data)
 }
