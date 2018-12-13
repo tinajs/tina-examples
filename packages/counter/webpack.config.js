26e77ea8ba09922beb0a5014980897ae668d5508
yelo <zhihuzeye@gmail.com>
2018-12-13 15:32:58 +0800

    feat(counter): upgrade to webpack 4

---
+++ packages/counter/webpack.config.js
@@ -0,0 +1,69 @@
+const { resolve } = require('path')
+const MinaEntryPlugin = require('@tinajs/mina-entry-webpack-plugin')
+const MinaRuntimePlugin = require('@tinajs/mina-runtime-webpack-plugin')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const loaders = {
+  script: 'babel-loader',
+  style: {
+    loader: 'postcss-loader',
+    options: {
+      config: {
+        path: resolve('./postcss.config.js'),
+      },
+    },
+  },
+}
+
+module.exports = {
+  context: resolve('src'),
+  entry: './app.mina',
+  output: {
+    path: resolve('dist'),
+    filename: '[name]',
+    publicPath: '/',
+    globalObject: 'wx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mina$/,
+        use: [{
+          loader: '@tinajs/mina-loader',
+          options: {
+            loaders,
+          },
+        }],
+      },
+      {
+        test: /\.png$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: 'assets/[name].[hash:6].[ext]',
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    symlinks: true,
+  },
+  plugins: [
+    new MinaEntryPlugin(),
+    new MinaRuntimePlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: 'common.js',
+      minChunks: 2,
+      minSize: 0,
+    },
+    runtimeChunk: {
+      name: 'runtime.js',
+    },
+  },
+  mode: isProduction ? 'production' : 'none',
+}
